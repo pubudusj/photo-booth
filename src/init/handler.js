b87d4ddfb4714f86e7295b8b1ea7dac7be4a37cb
Pubudu Jayawardana <pubudusj@gmail.com>
2021-04-21 17:15:30 +0200

    init function modified

--- src/init/handler.js
+++ src/init/handler.js
@@ -1,29 +1,24 @@
 'use strict';
-const AWS = require('aws-sdk');
-const stepfunctions = new AWS.StepFunctions();
+
+const AWS = require('aws-sdk')
+const stepfunctions = new AWS.StepFunctions()
 
 module.exports.index = async (event) => {
-  var input = {
+  let input = {
     bucketName: event['Records'][0]['s3']['bucket']['name'],
     bucektArn: event['Records'][0]['s3']['bucket']['arn'],
     object: event['Records'][0]['s3']['object']['key'],
+    fileName: event['Records'][0]['s3']['object']['key'].replace('uploads/', ''),
   }
 
-  var params = {
+  let params = {
     stateMachineArn: process.env['stateMachine'],
     input: JSON.stringify(input)
-  };
+  }
 
-  console.log(params);
-
-  await stepfunctions.startExecution(params, function(err, data) {
-    if (err) {
-      console.log(err, err.stack);
-    }
-    else  {
-      console.log(data);
-    }
-  }).promise();
-
-  return {success: true}
-};
+  try {
+    return stepfunctions.startExecution(params).promise()
+  } catch(err) {
+    console.log(err)
+  }
+}
