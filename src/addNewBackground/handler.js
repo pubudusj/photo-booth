0c8b1c1f6c79bf4049a39a333763a24e734e28f6
Pubudu Jayawardana <pubudusj@gmail.com>
2021-04-27 01:30:12 +0200

    Add new background functionality added

--- src/addNewBackground/handler.js
+++ src/addNewBackground/handler.js
@@ -1,6 +1,52 @@
-'use strict';
+'use strict'
+
+const AWS = require("aws-sdk")
+const s3 = new AWS.S3()
+const jimp = require('jimp')
+const fs = require('fs')
 
 module.exports.index = async event => {
-  console.log(event)
-  return { message: 'New Background Added', event };
-};
+  //Get image object
+  const image = await s3.getObject({
+    Bucket: event.bucketName,
+    Key: event.object 
+  }).promise()
+
+  //Get random background
+  var max = process.env['noOfBackgroundImages']
+  var rand = Math.floor(Math.random() * (max - 1 + 1)) + 1
+  console.log(rand)
+  const background = await s3.getObject({
+      Bucket: event.bucketName,
+      Key: 'backgrounds/background' + rand + '.png'
+    }).promise()
+
+  // Create jimp objects
+  const imageObj = await jimp.read(image.Body)
+  const backgroundObj = await jimp.read(background.Body)
+
+  // Merge image with background
+  const merged = await backgroundObj.composite(imageObj, 0, 0, { mode: jimp.BLEND_SOURCE_OVER })
+  
+  // Save merged image to tmp file
+  const outputFilePath = '/tmp/' + event.object
+  await merged.writeAsync(outputFilePath)
+
+  // Upload merged image to s3
+  var params = {
+    Bucket: event.bucketName,
+    Key: "new_background/" + event.fileName + ".png",
+    Body: fs.readFileSync(outputFilePath),
+    ContentType: "image/png",
+  }
+
+  await s3
+    .putObject(params)
+    .promise()
+
+    return {
+      bucketName: event.bucketName,
+      object: params.Key,
+      fileName: event.fileName,
+    }
+}
--- src/removeBackground/handler.js
+++ src/removeBackground/handler.js
@@ -1,19 +1,21 @@
-"use strict";
+"use strict"
 
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
-const axios = require("axios").default;
-const signedUrlExpireSeconds = 60 * 2;
+const AWS = require("aws-sdk")
+const s3 = new AWS.S3()
+const axios = require("axios").default
+const signedUrlExpireSeconds = 60 * 2
 
 module.exports.index = async (event) => {
-  var params = { Bucket: event.bucketName, Key: event.object };
+  var params = { Bucket: event.bucketName, Key: event.object }
 
+  // Generate presigned url for API
   var url = s3.getSignedUrl("getObject", {
     Bucket: event.bucketName,
     Key: event.object,
     Expires: signedUrlExpireSeconds,
-  });
+  })
 
+  // Call remove background API
   let response = await axios.post(
     "https://api.remove.bg/v1.0/removebg",
     {
@@ -27,23 +29,26 @@ module.exports.index = async (event) => {
       },
       encoding: null,
     }
-  );
+  )
+  
+  // Convert output (base64 string) to buffer
+  var buf = Buffer.from(response.data.data.result_b64, "base64")
 
-  var buf = Buffer.from(response.data.data.result_b64, "base64");
+  // Save image to s3
   var params = {
     Bucket: event.bucketName,
     Key: "background_removed/" + event.fileName + ".png",
     Body: buf,
     ContentType: "image/png",
-  };
+  }
 
   await s3
     .putObject(params)
-    .promise();
+    .promise()
 
   return {
     bucketName: event.bucketName,
     object: params.Key,
     fileName: event.fileName,
-  };
-};
+  }
+}
